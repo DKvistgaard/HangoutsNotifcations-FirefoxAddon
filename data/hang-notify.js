d7ae1b7a55ed733558aca7c48ab945460ef90049
Daniel Kvistgaard <dk@html24.net>
2014-10-23 00:03:18 +0200

    Checking for new messages in the chat.
    Currently it only console logs when a new message has been detected.

--- data/hang-notify.js
+++ data/hang-notify.js
@@ -1,1 +1,31 @@
-console.log("matches");
+(function($) {
+
+    $(document).on('ready', function() {
+        setTimeout(function() {}, 5000);
+        setInterval(checkNewMessage, 500);
+
+        window.windowFocus = false;
+        $(window).focus(function() {
+            window.windowFocus = true;
+        }).blur(function() {
+            window.windowFocus = false;
+        });
+    });
+
+    window.oldDivLength = -1;
+    window.currentDivLength = -1;
+    function checkNewMessage() {
+        var divLength = $('div').length;
+        if (window.oldDivLength == -1) {
+            window.oldDivLength = divLength;
+        }
+        window.currentDivLength = divLength;
+        if (window.currentDivLength > window.oldDivLength && window.oldDivLength !== 0) {
+            if (!window.windowFocus) {
+                console.log('new message!!');
+            }
+            window.oldDivLength = window.currentDivLength;
+        }
+    }
+
+})(jQuery);
