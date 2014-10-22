62190a4e4eb54352131d4585c8d640d48188135f
Daniel Kvistgaard <dk@html24.net>
2014-10-23 00:29:05 +0200

    Creating desktop notification.
    Using the last message in the chat to create the notification.

--- data/hang-notify.js
+++ data/hang-notify.js
@@ -1,19 +1,29 @@
 (function($) {
 
-    $(document).on('ready', function() {
-        setTimeout(function() {}, 5000);
-        setInterval(checkNewMessage, 500);
-
-        window.windowFocus = false;
-        $(window).focus(function() {
-            window.windowFocus = true;
-        }).blur(function() {
-            window.windowFocus = false;
-        });
-    });
-
     window.oldDivLength = -1;
     window.currentDivLength = -1;
+
+    function createNotification() {
+        var message = $('div.Mu.SP:last').text();
+        var user = $('div.UR.UG:last').text();
+        var title = "Hangouts: " + user;
+        var options = {
+            body: message
+        };
+        if (Notification.permission === "granted") {
+            var notification = new Notification(title, options);
+        } else if (Notification.permission !== "denied") {
+            Notification.requestPermission(function(permission) {
+                if (!('permission' in Notification)) {
+                    Notification.permission = permission;
+                }
+                if (permission === "granted") {
+                    var notification = new Notification(title, options);
+                }
+            });
+        }
+    }
+
     function checkNewMessage() {
         var divLength = $('div').length;
         if (window.oldDivLength == -1) {
@@ -22,10 +32,22 @@
         window.currentDivLength = divLength;
         if (window.currentDivLength > window.oldDivLength && window.oldDivLength !== 0) {
             if (!window.windowFocus) {
-                console.log('new message!!');
+                createNotification();
             }
             window.oldDivLength = window.currentDivLength;
         }
     }
+
+    $(document).on('ready', function() {
+        setTimeout(function() {}, 5000);
+        setInterval(checkNewMessage, 100);
+
+        window.windowFocus = false;
+        $(window).focus(function() {
+            window.windowFocus = true;
+        }).blur(function() {
+            window.windowFocus = false;
+        });
+    });
 
 })(jQuery);
